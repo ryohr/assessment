fce7fdba99d88245672a2dcd946a26eb5a509d4c
Rei <26517554+ryohr@users.noreply.github.com>
2020-05-31 15:28:55 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -15,6 +15,13 @@ function removeAllChildren(element) {
   }
 }
 
+// enter keyでも結果を取得できるようにする
+userNameInput.onkeydown = event => {
+  if (event.key === 'Enter') {
+    assessmentButton.onclick();
+  }
+}
+
 assessmentButton.onclick = () => {
   const userName = userNameInput.value;
   if (userName.length === 0) {
